43c80b394e5b218aa37cbbc4f8e911f933474e13
Brenno Souza <brennod17@icloud.com>
2025-10-29 16:44:53 -0300

    feat: create error pages for 404, 422, and 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  get 'errors/unprocessable_entity'
   root "pages#index"
 
   get "sobre", to: "pages#sobre"
   get "contato", to: "pages#contato"
 
   resources :seguros, only: [:index, :show]
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  match "/422", to: "errors#unprocessable_entity", via: :all
 
   namespace :admin do
     root "dashboard#index"
